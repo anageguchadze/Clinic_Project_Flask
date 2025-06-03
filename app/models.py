cafc7554a55b67f6da48fab2e28395511e0884c5
Ana <anageguchadze22@gmail.com>
2025-06-03 16:06:47 +0400

    Add db for users

---
+++ app/models.py
@@ -0,0 +1,20 @@
+# app/models.py
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    role = db.Column(db.String(10), nullable=False)  # 'patient' or 'doctor'
+
+class Appointment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    service = db.Column(db.String(150), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship('User', backref='appointments')
